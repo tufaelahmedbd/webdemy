c3e9daaaa91511b999dbab5360b70b0652905f2b
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-05-03 13:11:17 +0600

    courses api added

--- prisma/courses.js
+++ prisma/courses.js
@@ -1,14 +1,17 @@
 import prisma from "./prisma";
-
-//Get all courses
+//C
+//Get all courses from DB
 export const getAllCourses = async () => {
   const courses = await prisma.course.findMany({});
+
   return courses;
 };
 
-//Get a single course
+//Get a single course from DB
 export const getCourse = async (id) => {
   const course = await prisma.course.findUnique({
-    where: { id: id },
+    where: { id },
   });
+
+  return course;
 };
